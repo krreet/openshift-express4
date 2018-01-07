a93a16cdc950ea7a286e91aa967b6f6465844760
Reet <reet1plus1@gmail.com>
2018-01-07 23:49:35 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-/**
- * Created by atulr on 05/07/15.
- */
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('express-handlebars');
+
 var routes = require('./routes/index');
 
 var app = express();
 
+// view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
@@ -27,13 +33,16 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-//================
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send('error'+ err.message );
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
 }
 
@@ -41,7 +50,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('error'+ err.message );
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
-/**
- * Created by atulr on 05/07/15.
- */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('index :D');
+  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/test/:id', function(req, res, next) {
+  res.render('test', {output: req.params.id});
+});
+
+router.post('/test/submit', function(req, res, next) {
+  res.redirect('/test/...');
 });
 
 module.exports = router;
